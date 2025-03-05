aac9303a710392de853f0b7152d429f1905ec6ff
MahmoudHassan12 <mahmoudsbongepop@gmail.com>
2025-03-05 22:49:05 +0200

    added snackbar for sign in

--- lib/features/auth/presentation/views/widgets/sign_in/sign_in_form.dart
+++ lib/features/auth/presentation/views/widgets/sign_in/sign_in_form.dart
@@ -43,17 +43,31 @@ class _SignInFormState extends State<SignInForm> {
         log('Sign-in successful: ${user.uid}');
 
         if (mounted) {
+          _showSnackbar('Welcome back!', true);
           await handleSignInSuccess();
         }
       } else {
         log('Sign-in failed');
+        _showSnackbar('Sign-in failed. Please try again.', false);
       }
     } catch (e) {
       log('Sign-in Error: $e');
+      _showSnackbar('An error occurred: $e', false);
     } finally {
       if (mounted) setState(() => _isLoading = false);
     }
   }
+
+  void _showSnackbar(String message, bool flag) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: flag ? Colors.green[500] : Colors.red[500],
+        duration: const Duration(seconds: 3),
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
