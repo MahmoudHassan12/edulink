19b7457409146beca78d5cf05e56dca25b75800e
YousefSaber128 <yousef.saber128@gmail.com>
2025-03-05 21:06:44 +0200

    Implement course registration view; add user data fetching, display courses in a list, and include a register button

--- lib/features/register_courses/presentation/views/widgets/register_courses_view_body.dart
+++ lib/features/register_courses/presentation/views/widgets/register_courses_view_body.dart
@@ -1,12 +1,60 @@
+import 'package:cached_network_image/cached_network_image.dart'
+    show CachedNetworkImageProvider;
+import 'package:edu_link/core/constants/borders.dart';
+import 'package:edu_link/core/helpers/get_user.dart';
+import 'package:edu_link/core/widgets/buttons/custom_filled_button.dart';
 import 'package:edu_link/core/widgets/e_text.dart';
 import 'package:flutter/material.dart';
 
 class RegisterCoursesViewBody extends StatelessWidget {
   const RegisterCoursesViewBody({super.key});
   @override
-  Widget build(BuildContext context) => const CustomScrollView(
+  Widget build(BuildContext context) => CustomScrollView(
     slivers: [
-      SliverAppBar(title: EText('Register Courses'), centerTitle: true),
+      const SliverAppBar(title: EText('Register Courses'), centerTitle: true),
+      SliverPadding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        sliver: FutureBuilder(
+          future: getUser(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const SliverToBoxAdapter(
+                child: Center(child: CircularProgressIndicator()),
+              );
+            }
+            final courses = snapshot.data?.courses;
+            return SliverList.builder(
+              itemCount: courses?.length ?? 0,
+              itemBuilder: (context, index) {
+                final course = courses?[index];
+                return Card.filled(
+                  margin: const EdgeInsets.symmetric(vertical: 4),
+                  shape: const RoundedRectangleBorder(borderRadius: sBorder),
+                  clipBehavior: Clip.antiAlias,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: CachedNetworkImageProvider(
+                        course!.imageUrl!,
+                      ),
+                      radius: 20,
+                    ),
+                    title: EText(course.code!),
+                    subtitle: EText(course.title!),
+                    trailing: Checkbox(value: false, onChanged: (_) {}),
+                    onTap: () {},
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+      SliverToBoxAdapter(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: CustomFilledButton(label: 'Register', onPressed: () {}),
+        ),
+      ),
     ],
   );
 }
