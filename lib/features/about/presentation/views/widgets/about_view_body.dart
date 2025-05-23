179c7a9d1476bd91d9a368643cba976c5faeb70f
Yousef Saber <yousef.saber128@gmail.com>
2025-05-23 12:30:30 +0300

    Refactor AboutViewBody to use CustomScrollView for improved layout; enhance code readability and maintainability.

--- lib/features/about/presentation/views/widgets/about_view_body.dart
+++ lib/features/about/presentation/views/widgets/about_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:edu_link/core/constants/borders.dart';
 import 'package:edu_link/core/widgets/e_text.dart';
 import 'package:flutter/material.dart';
 
@@ -5,88 +6,64 @@ class AboutViewBody extends StatelessWidget {
   const AboutViewBody({super.key});
 
   @override
-  Widget build(BuildContext context) => SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-      child: Column(
-        children: [
-          const CircleAvatar(
-            radius: 60,
-            backgroundImage: AssetImage('assets/images/logo.png'),
-            backgroundColor: Colors.transparent,
-          ),
-          const SizedBox(height: 12),
-          const Text(
-            '''
+  Widget build(BuildContext context) {
+    TableRow row(String name) => TableRow(
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(12),
+          child: Icon(Icons.person, color: Color.fromARGB(255, 11, 103, 223)),
+        ),
+        Padding(padding: const EdgeInsets.all(12), child: Text(name)),
+      ],
+    );
+    return CustomScrollView(
+      slivers: [
+        SliverPadding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
+          sliver: SliverList.list(
+            children: [
+              const Center(
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage('assets/images/logo.png'),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                '''
 EduLink is a smart university platform that bridges communication between students and professors.
 It allows students to apply for courses, access organized course materials, participate in course-specific Q&A forums, and chat 1-to-1 with professors.
 Professors can manage their courses, moderate discussions, and customize their profiles.
 EduLink simplifies academic interaction and brings everything students need into one place.''',
-            textAlign: TextAlign.justify,
-            style: TextStyle(fontSize: 16, height: 1.6),
-          ),
-          const SizedBox(height: 32),
-          const EText(
-            'Developed By',
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 16),
-          Table(
-            border: TableBorder.all(
-              color: const Color.fromARGB(255, 41, 41, 41),
-              width: 1.2,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-            columnWidths: const {0: FlexColumnWidth(0.2), 1: FlexColumnWidth()},
-            children: const [
-              TableRow(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(12),
-                    child: Icon(
-                      Icons.person,
-                      color: Color.fromARGB(255, 11, 103, 223),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(12),
-                    child: Text('Hossam Hassan Ibrahem'),
-                  ),
-                ],
+                style: TextStyle(fontSize: 16, height: 1.6),
               ),
-              TableRow(
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(12),
-                    child: Icon(
-                      Icons.person,
-                      color: Color.fromARGB(255, 11, 103, 223),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(12),
-                    child: Text('Mahmoud Hassan Samir'),
-                  ),
-                ],
+              const SizedBox(height: 32),
+              const EText(
+                'Developed By',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
               ),
-              TableRow(
+              const SizedBox(height: 16),
+              Table(
+                border: TableBorder.all(
+                  color: const Color.fromARGB(255, 41, 41, 41),
+                  width: 1.2,
+                  borderRadius: xxsBorder,
+                ),
+                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                columnWidths: const {
+                  0: FlexColumnWidth(0.2),
+                  1: FlexColumnWidth(),
+                },
                 children: [
-                  Padding(
-                    padding: EdgeInsets.all(12),
-                    child: Icon(
-                      Icons.person,
-                      color: Color.fromARGB(255, 11, 103, 223),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(12),
-                    child: Text('Yousef Saber Mohammad Kamal'),
-                  ),
+                  row('Hossam Hassan Ibrahem'),
+                  row('Mahmoud Hassan Samir'),
+                  row('Yousef Saber Mohammad Kamal'),
                 ],
               ),
             ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
+  }
 }
