a8a43e292e9421de534c8367a24688d4239e11f6
Yousef Saber <yousef.saber128@gmail.com>
2025-06-18 00:43:32 +0300

    Enhance refresh logic in HomeViewBody to update user data from Firestore

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,5 @@
+import 'package:edu_link/core/helpers/get_user.dart';
+import 'package:edu_link/core/repos/user_repo.dart';
 import 'package:edu_link/features/home/presentation/controllers/home_cubit/home_cubit.dart'
     show HomeCubit;
 import 'package:edu_link/features/home/presentation/views/widgets/current_courses.dart';
@@ -10,7 +12,13 @@ class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
   @override
   Widget build(BuildContext context) => RefreshIndicator(
-    onRefresh: () => context.read<HomeCubit>().getCourses(),
+    onRefresh: () => const UserRepo()
+        .getFromFireStore(documentId: getUser!.id!)
+        .then((user) => const UserRepo().udateLocal(user!.toMap()))
+        .then(
+          (_) =>
+              context.mounted ? context.read<HomeCubit>().getCourses() : null,
+        ),
     child: CustomScrollView(
       slivers: [
         const HomeAppBar(),
